398304208c8eda3e85252d8b268d2f18b36178d0
bsdcpp <bsdcpp@gmail.com>
2020-05-25 21:12:45 +0800

    Update aba.js

--- aba.js
+++ aba.js
@@ -1,11 +1,18 @@
 var body = $response.body;
+var url = $request.url;
+
 
 let obj = JSON.parse(body);
 
-obj.activities.forEach((book, index)=> {
-    book.active = true;
-    book.blockedBy = [];
-  });
+if (url.indexOf("activities") != -1) {
+    obj.active = true;
+    obj.blockedBy = [];
+} else {
+    obj.activities.forEach((book, index)=> {
+        book.active = true;
+        book.blockedBy = [];
+      });
+}
 body = JSON.stringify(obj);  
 
 $done({body});
