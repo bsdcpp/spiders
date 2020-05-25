99cc6d573e73ac2852715a640dea5d94aaa2c135
bsdcpp <bsdcpp@gmail.com>
2020-05-25 22:05:55 +0800

    Update wf.js

--- wf.js
+++ wf.js
@@ -1,13 +1,18 @@
 var body = $response.body;
 var url = $request.url;
-const path = "/course/courseInfo";
+const course = "/course/courseInfo";
+const enti = "course/courseChapter20";
 
 let obj = JSON.parse(body);
 
-if (url.indexOf(path) != -1) {
+if (url.indexOf(course) != -1) {
     obj.chapterlist.forEach((book, index)=> {
         book.belongFlag = 1;
         book.chaptertype= 1;
+      });
+} else if(url.indexOf(enti) != -1) {
+    obj.courseChapterEntity.forEach((book, index)=> {
+        book.chaptertype = 1;
       });
 }
 body = JSON.stringify(obj);  
