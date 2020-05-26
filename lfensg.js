5aa5ba22bf6fd94028f0d74358bd4712a2ec3dfc
bsdcpp <bsdcpp@gmail.com>
2020-05-26 22:47:17 +0800

    Update lfensg.js

--- lfensg.js
+++ lfensg.js
@@ -5,9 +5,11 @@ const dsong = "/api/v1/contents/song";
 const cid = "/api/v1/contents/C0";
 
 let obj = JSON.parse(body);
-console.log(url);
+
 if (url.indexOf(dstory) != -1 || url.indexOf(dsong) != -1) {
+    console.log("111111");
     if (obj.data.hasOwnProperty('contents')) {
+        console.log("222222");
         obj.data.contents.forEach((book, index)=> {
             //book.service_info.ebook         = 'F';
             //book.service_info.original_text = 'F';
@@ -20,8 +22,10 @@ if (url.indexOf(dstory) != -1 || url.indexOf(dsong) != -1) {
     body = JSON.stringify(obj);  
 }
 if (url.indexOf(cid) != -1 && url.indexOf("player") != -1) {
+    console.log("33333");
     delete obj.data.preview_time;
     if (obj.data.hasOwnProperty('next_content')) {
+        console.log("44444");
         book = obj.data.next_content;
         //book.service_info.ebook         = 'F';
         //book.service_info.original_text = 'F';
@@ -33,6 +37,6 @@ if (url.indexOf(cid) != -1 && url.indexOf("player") != -1) {
     body = JSON.stringify(obj);  
 }
 
-
+console.log("5555555");
 
 $done({body});
