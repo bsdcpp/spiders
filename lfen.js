287e5dd07831320be86a7d72a33470a052d3d330
bsdcpp <bsdcpp@gmail.com>
2020-05-26 10:32:19 +0800

    Update lfen.js

--- lfen.js
+++ lfen.js
@@ -1,33 +1,20 @@
-var body = $response.body;
-var url = $request.url;
-const did = "/api/v1/contents/story";
-const cid = "/api/v1/contents/C0";
+let body = $response.body
+let url  = $request.url;
+obj = JSON.parse(body)
 
-let obj = JSON.parse(body);
-
-if (url.indexOf(did) != -1) {
-    obj.data.contents.forEach((book, index)=> {
-        book.service_info.ebook         = 'F';
-        book.service_info.original_text = 'F';
-        book.service_info.quiz          = 'F';
-        book.service_info.service       = 'F';
-        book.service_info.story         = 'F';
-        book.service_info.vocabulary    = 'F';
+if (url.indexOf("/api/v1/contents/story") != -1) {
+    obj['data']['contents'].forEach((book, index)=> {
+        book['service_info.service']       = 'F';
+        book['service_info.story']         = 'F';
       });
     body = JSON.stringify(obj);  
 }
-if (url.indexOf(cid) != -1 && url.indexOf("player") != -1) {
-    delete obj.data.preview_time;
-    book = obj.data.next_content;
-    book.service_info.ebook         = 'F';
-    book.service_info.original_text = 'F';
-    book.service_info.quiz          = 'F';
-    book.service_info.service       = 'F';
-    book.service_info.story         = 'F';
-    book.service_info.vocabulary    = 'F';
+if (url.indexOf("/api/v1/contents/C0") != -1 && url.indexOf("player") != -1) {
+    delete obj['data.preview_time'];
+    book = obj['data.next_content'];
+    book['service_info.service']       = 'F';
+    book['service_info.story']         = 'F';
     body = JSON.stringify(obj);  
 }
-
-
 
 $done({body});
