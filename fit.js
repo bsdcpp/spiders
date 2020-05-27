29e202fa1660300c51ed9bf94f4a04bfeb653868
bsdcpp <bsdcpp@gmail.com>
2020-05-27 22:42:09 +0800

    Create fit.js

---
+++ fit.js
@@ -0,0 +1,12 @@
+/*
+fit会员课程解锁by lovexl
+qq群：1034788765
+github：https://github.com/yukerui/Loon-scripts
+^https:\/\/bea\.sportq\.com\/SFitWeb\/sfit\/getUserBaseInfo
+[mitm] hostname= bea.sportq.com
+ */
+let obj=JSON.parse($response.body)
+if (obj.entRet.entUserBaseInfo['isVip']!=null){
+    obj.entRet.entUserBaseInfo['isVip'] = 1;
+}
+$done({body: JSON.stringify(obj)});
