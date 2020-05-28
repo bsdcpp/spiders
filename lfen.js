fa7d89295b02ba85f48fbbe9ca119113dea26a55
bsdcpp <bsdcpp@gmail.com>
2020-05-28 10:32:52 +0800

    Update lfen.js

--- lfen.js
+++ lfen.js
@@ -1,13 +1,5 @@
 var body = $response.body;
-var url = $request.url;
-const dstory = "/api/v1/contents/story";
-const dsong = "/api/v1/contents/song";
-const cid = "/api/v1/contents/C0";
 
 let obj = JSON.parse(body);
-
-
-//let obj2 = JSON.parse(body);
-//console.log(obj2.data.contents[10].service_info);
 
 $done({body});
