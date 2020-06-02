64b749bea962f7aa25dd4d3fa8f993322d944818
bsdcpp <bsdcpp@gmail.com>
2020-06-02 12:08:15 +0800

    Update abaen_req.js

--- abaen_req.js
+++ abaen_req.js
@@ -3,6 +3,7 @@ var url = $request.url;
 if (url.search('(/learningpath/api/v2/units/[0-9]+/activities/[0-9_]+)') != -1) {
     $request.url = "http://192.168.2.248:555/abaEnglish" + RegExp.$1;
     url = $request.url;
+    console.log(url)
 } 
 
-$done({url});
+$done({});
