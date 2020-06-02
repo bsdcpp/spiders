ace07d167314d7bc31643ed33df643b6def71e71
bsdcpp <bsdcpp@gmail.com>
2020-06-02 22:28:23 +0800

    Update lfen.js

--- lfen.js
+++ lfen.js
@@ -2,7 +2,9 @@ var body = $response.body;
 var url = $request.url;
 
 
+console.log("aaaaaaaaaaaaa");
 if (url.search("/api/v1/auth/me") != -1) {
+    console.log("bbbbbbbbbbbbbbbbbb");
     let obj = JSON.parse(body);
     obj.data.remaining_day = 300;
     
