949589f5d3532c7b7b85470ef92a4e3cd5377088
bsdcpp <bsdcpp@gmail.com>
2020-06-02 11:26:19 +0800

    Update abaen.js

--- abaen.js
+++ abaen.js
@@ -3,23 +3,29 @@ var url = $request.url;
 const mep = "/api/v2/users/me";
 const unit = "/learningpath/api/v2/units";
 
-let obj = JSON.parse(body);
+
 
 if (url.indexOf(mep) != -1) {
+    let obj = JSON.parse(body);
     obj.expirationDate = "2021-05-17T09:53:34";
     obj.type = 2;
+    body = JSON.stringify(obj);  
 }
 
-if (url.indexOf("activities") != -1) {
+if (url.search('/learningpath/api/v2/units/[0-9]+/activities/[0-9_]+') != -1) {
+    let obj = JSON.parse(body);
     obj.active = true;
     obj.blockedBy = [];
+    body = JSON.stringify(obj);  
 } 
 if (url.indexOf(unit) != -1) {
+    let obj = JSON.parse(body);
     obj.activities.forEach((book, index)=> {
         book.active = true;
         book.blockedBy = [];
       });
+    body = JSON.stringify(obj);  
 }
-body = JSON.stringify(obj);  
+
 
 $done({body});
