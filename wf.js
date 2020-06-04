8a58a33fca7c797597d4640e09599679eb53cfdf
bsdcpp <bsdcpp@gmail.com>
2020-06-04 19:39:57 +0800

    Update wf.js

--- wf.js
+++ wf.js
@@ -2,20 +2,32 @@ var body = $response.body;
 var url = $request.url;
 const course = "/course/courseInfo";
 const enti = "/course/courseChapter";
+const vip = "/vip/queryVipInfo";
 
-let obj = JSON.parse(body);
+
 
 if (url.indexOf(course) != -1) {
-    obj.chapterlist.forEach((book, index)=> {
+    let obj = JSON.parse(body);
+    obj.chapterlist.forEach((book, index)=> {vipEndTime = 
         book.belongFlag = 1;
         book.chaptertype= 1;
       });
     body = JSON.stringify(obj);  
 } 
 if(url.indexOf(enti) != -1) {
+    let obj = JSON.parse(body);
     obj.courseChapterEntity.chaptertype = 1;
     body = JSON.stringify(obj);  
 }
+
+if(url.indexOf(vip) != -1) {
+    let obj = JSON.parse(body);
+    obj.memberEntity.vipType = 1;
+    obj.memberEntity.vipStartTime = 1546879999000;
+    obj.memberEntity.vipEndTime = 1626879999000;
+    body = JSON.stringify(obj);  
+}
+
 
 
 $done({body});
