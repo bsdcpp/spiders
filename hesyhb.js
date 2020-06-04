42c50eba9d47be59d1aa0d47f394bf593d9af192
bsdcpp <bsdcpp@gmail.com>
2020-06-04 11:17:41 +0800

    Update hesyhb.js

--- hesyhb.js
+++ hesyhb.js
@@ -1,13 +1,13 @@
 var body = $response.body;
 var url = $request.url;
 
-if (url.indexOf("get_book_info") != -1 ) {
+
     let obj = JSON.parse(body);
     obj.result.books.forEach((book, index)=> {
         book.is_vip = 0;
       });
     body = JSON.stringify(obj);
-}
+
 
 
 
