b9fb4825e440910e2cc1472a988529e243d0063c
bsdcpp <bsdcpp@gmail.com>
2020-06-04 16:33:33 +0800

    Update kada.js

--- kada.js
+++ kada.js
@@ -9,4 +9,23 @@ if (url.indexOf("getCollectItems.json") != -1) {
     body = JSON.stringify(obj);
 }
 
+if (url.indexOf("getUserDetail.json") != -1) {
+    let obj = JSON.parse(body);
+    obj.data.vipInfo.level = 99;
+    obj.data.vipInfo.status = 1;
+    obj.data.vipInfo.vipType = 1;
+    obj.data.vipInfo.payFlag = 1;
+    obj.data.vipInfo.endTime = 1646879999000;
+    obj.data.vipInfo.withholdingTime = 1647879999000;
+
+    
+    obj.data.vipInfoV2.level = 99;
+    obj.data.vipInfoV2.status = 1;
+    obj.data.vipInfoV2.vipType = 1;
+    obj.data.vipInfoV2.payFlag = 1;
+    obj.data.vipInfoV2.endTime = 1646879999000;
+    obj.data.vipInfoV2.withholdingTime = 1647879999000;
+    body = JSON.stringify(obj);
+}
+
 $done({body});
