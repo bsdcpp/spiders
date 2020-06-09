b7b3c71011f56262cc05d264fba74e2de8324850
bsdcpp <bsdcpp@gmail.com>
2020-06-09 22:58:48 +0800

    Create ivy.js

---
+++ ivy.js
@@ -0,0 +1,12 @@
+
+
+var body = $response.body;
+var url = $request.url;
+
+if (url.indexOf("/api/mobile/user/login/loginByPhoneToken") != -1) {
+    let obj = JSON.parse(body);
+    obj.vip = True;
+    body = JSON.stringify(obj);
+}
+
+$done({body});
