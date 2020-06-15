3c2f55adab81cd8e0c15e596cab0c6e071d693ff
bsdcpp <bsdcpp@gmail.com>
2020-06-15 23:56:15 +0800

    Update huhu.js

--- huhu.js
+++ huhu.js
@@ -12,8 +12,8 @@ if (url.search("cateStoryList\.html") != -1) {
 
 if (url.search("getVipDate\.html") != -1) {
         let obj = JSON.parse(body);
-    obj.vip_start_date = "2020-01-01";
-    obj.vip_end_date = "2040-01-01";
+        obj.msg.vip_start_date = "2020-01-01";
+        obj.msg.vip_end_date = "2040-01-01";
         body = JSON.stringify(obj);
 }
 $done({body});
